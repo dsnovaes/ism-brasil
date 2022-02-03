b7c5a94706ee324fbb6227aef40bd486d2c51319
Diego Novaes <dsnovaes1@gmail.com>
2022-02-03 19:30:13 -0300

    alteracao

--- template-home.php
+++ template-home.php
@@ -10,7 +10,7 @@
 */
 get_header();
 ?>
-<div class="container">
+<div class="container">.
     <div class="row">
         <div class="col-12 greenGradient mb-5 py-3 mt-3 title bannerHome">
             <div class="col-11 col-md-10 mx-auto">
