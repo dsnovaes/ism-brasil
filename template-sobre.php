1e15603348b3b98a06aeb4a2beb01e1fabee5a3b
Diego Novaes <dsnovaes1@gmail.com>
2022-02-09 16:59:45 -0300

    template sobre

--- template-sobre.php
+++ template-sobre.php
@@ -24,12 +24,19 @@ get_header();
 <div class="container">
     <div class="row">
         <div class="col-11 col-md-10 mx-auto">
-            <div class="col-3"><?php the_field('intro') ?></div>
-            <div class="col-9"><?php the_post_thumbnail( array( 730,400 ) ); ?></div>
+            <div class="row">
+                <div class="col-4"><p><?php the_field('intro') ?></p></div>
+                <div class="col-8"><?php the_post_thumbnail( array( 730,400 ) ); ?></div>
+            </div>
         </div>
     </div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <div class="col-11 col-md-8 mx-auto"><?php the_field('text_center') ?></div>
+    </div>
+</div>
             
 <?php
 get_footer();
