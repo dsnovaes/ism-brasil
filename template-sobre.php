7200f41bec74df7a53a252680180d57c5624ac9d
Diego Novaes <dsnovaes1@gmail.com>
2022-02-09 17:52:22 -0300

    timeline no sobre

--- template-sobre.php
+++ template-sobre.php
@@ -41,31 +41,60 @@ get_header();
 <div class="container">
     <div class="row my-5">
         <div class="col-11 col-md-12 mx-auto">
-        <h2>Nossa história</h2>    
-        <?php
+            <h2>Nossa história</h2>   
+            <div class="swiper-timeline">
+                <div class="swiper-wrapper"> 
+            <?php
 
-        // Check rows exists.
-        if( have_rows('timeline') ):
+            // Check rows exists.
+            if( have_rows('timeline') ):
 
-            // Loop through rows.
-            while( have_rows('timeline') ) : the_row();
+                // Loop through rows.
+                while( have_rows('timeline') ) : the_row();
 
-                // Load sub field value.
-                $sub_value = get_sub_field('timeline_year');
+                    // Load sub field value.
+                    $year = get_sub_field('timeline_year');
+                    $text = get_sub_field('timeline_text');
+                    $img = get_sub_field('timeline_img');
+                    // Do something...
+                    ?>
+                    <div class="swiper-slide"><?php echo $year; ?></div>
+                    <?php
+                // End loop.
+                endwhile;
+
+            // No value.
+            else :
                 // Do something...
-                echo $sub_value;
-            // End loop.
-            endwhile;
-
-        // No value.
-        else :
-            // Do something...
-        endif;
-        ?>
+            endif;
+            ?>
+                <div class="swiper-button-prev"></div>
+				<div class="swiper-button-next"></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
             
+
+<script>
+	const swiper = new Swiper('.swiper-timeline', {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: false,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    })
+
+</script>
+
 <?php
 get_footer();
 ?>
