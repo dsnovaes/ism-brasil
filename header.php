0c88643ab9c7fc3d1b48e006f4a959d3ac982bb0
Diego Novaes <dsnovaes1@gmail.com>
2022-08-10 22:10:24 -0300

    removes GTM code

--- header.php
+++ header.php
@@ -16,13 +16,6 @@
 
     <head>
         <title><?php echo wp_title(); ?></title>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5DZCGQ3');</script>
-        <!-- End Google Tag Manager -->
 
         <meta charset="utf-8">
 
@@ -60,10 +53,6 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZCGQ3"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <div class="veryTopBar orangeGradient"></div>
 <div class="container">
         <div class="col-12 mx-auto" id="menuSecondary">
