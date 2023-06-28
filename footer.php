3312b7b7623af7abb804f2e0772d71e3a4bb7797
Diego Novaes <dsnovaes1@gmail.com>
2023-06-28 14:31:31 -0300

    modal will only show on glow's page

--- footer.php
+++ footer.php
@@ -175,7 +175,9 @@ if ($args[0] == "glow") { ?>
 		modalAge.removeAttribute("style")
 		modalAge.setAttribute("style","display:none")
 	}
-
+</script>
+<?php }; ?>
+<script>
 	const btnTop = document.querySelector("footer .footnotes a.top")
 	const theTop = document.querySelector("body")
 	btnTop.addEventListener("click", (e) => {
@@ -183,7 +185,6 @@ if ($args[0] == "glow") { ?>
 		theTop.scrollIntoView()
 	})
 </script>
-<?php }; ?>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
